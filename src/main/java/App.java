f4e9cb12a7e4a3073a67fd82ac09b36775fab461
Rogovolod <Rogovolod@gmail.com>
2020-11-27 22:21:49 +0900

    Java pre-project. Практическая задача 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,22 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat bean3 = (Cat) applicationContext.getBean("cat");
+        Cat bean4 = (Cat) applicationContext.getBean("cat");
         System.out.println(bean.getMessage());
+        System.out.println(bean3.getSound());
+        if (bean.equals(bean2)) {
+            System.out.println("True. HW.");
+        } else {
+            System.out.println("False. HW.");
+        }
+        if (bean3.equals(bean4)) {
+            System.out.println("True. Cat.");
+        } else {
+            System.out.println("False. Cat.");
+        }
+
     }
 }
